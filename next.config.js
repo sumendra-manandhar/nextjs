b7f700c884051d2e6b938c1a7a28d9f564b9a479
sumendra-manandhar <thesume.2010@gmail.com>
2023-10-07 17:08:06 +0545

    IP

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+
+module.exports = {
+    // Other Next.js configurations...
+    async rewrites() {
+      return [
+        // Add any necessary rewrites here if needed
+      ];
+    },
+    async redirects() {
+      return [
+        // Add any necessary redirects here if needed
+      ];
+    },
+    async generateStaticParams() {
+      // Specify the paths of the pages you want to export
+      return [
+        // Example: { page: '/', query: {} },
+        // Add your pages here
+      ];
+    },
+  };
+  
